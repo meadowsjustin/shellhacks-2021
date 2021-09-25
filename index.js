b888c42c6cffadfe5d7ce429806cef8200e8605a
Rex Bianchi <rexbianchi@protonmail.com>
2021-09-25 15:34:56 -0400

    actual button code #1

---
+++ index.js
@@ -0,0 +1,6 @@
+const dealButton = document.getElementById("dealButton");
+
+button.addEventListener("click", function () {
+    dealButton = document.getElementById("dealButton").style.visibility = "hidden";
+
+});

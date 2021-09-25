e991ef008ac94dee8ba5c8c81ec576ad47c34400
Rex Bianchi <rexbianchi@protonmail.com>
2021-09-25 15:51:56 -0400

    bugfix

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const dealButton = document.getElementById("dealButton");
+// const hitButton = document.getElementById("hitButton");
+// const standButton = document.getElementById("standButton")
+
+let inGame = false;
+
+dealButton.addEventListener("click", function () {
+    dealButton = document.getElementById("dealButton").style.visibility = "hidden";
+    inGame = true;
+    console.log("The game is running");
+});
+/*
+button.addEventListener("click", function () {
+    hitButton = document.getElementById("hitButton")
+});
+
 
 button.addEventListener("click", function () {
-    dealButton = document.getElementById("dealButton").style.visibility = "hidden";
+    standButton = document.getElementById("standButton")
+
 
 });
+*/
